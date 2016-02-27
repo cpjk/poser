93bbcec955619d06507e8a7c6b853d25ff3587f5
Chris Kelly <cpjkelly@gmail.com>
2016-02-26 22:10:37 -0800

    Add project struct

---
+++ lib/pose/project.ex
@@ -0,0 +1,11 @@
+defmodule Project do
+  defstruct name: "", questions: []
+
+  def new(args) do
+    # create a new project
+  end
+
+  def add_question(question) do
+   # add a question to the project
+  end
+end
