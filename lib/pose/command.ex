beef2ffe8db44296ea66be08197e40120b7cfdb8
Chris Kelly <cpjkelly@gmail.com>
2016-02-26 22:13:03 -0800

    Add command skeleton

--- lib/pose/command.ex
+++ lib/pose/command.ex
@@ -1,4 +1,19 @@
 defmodule Command do
-  def parse(["new" | rest]), do: Question.new(rest)
-  def parse(["resolve" | rest]), do: Question.resolve(rest)
+  def parse(["new" | rest]), do: _new(rest)
+  def parse(["resolve" | rest]), do: _resolve_question(rest)
+
+  def _new(["project" | rest]), do: _new_project(rest)
+  def _new(["question" | rest]), do: _new_question(rest)
+
+  defp _resolve_question(args) do
+    # prompt for details
+  end
+
+  defp _new_question(args) do
+    # prompt for details
+  end
+
+  defp _new_project(args) do
+    # prompt for details
+  end
 end
